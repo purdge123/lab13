a99a7511940fb14b6d9cf21771918b108db7318a
purdge123 <122433726+purdge123@users.noreply.github.com>
2023-01-11 12:43:06 +0500

    Update test.java

--- LabQuiz/src/task3/test.java
+++ LabQuiz/src/task3/test.java
@@ -1,5 +1,14 @@
 package task3;
 
 public class test {
-
+   public static void main(String args[])
+    {
+        try
+        {
+            fun();
+        }
+        catch(IllegalAccessException e)
+        {
+            System.out.println("caught in main.");
+        }
 }
